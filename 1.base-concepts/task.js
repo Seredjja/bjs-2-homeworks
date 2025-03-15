131da19924cb855220e9ad842b5899527796c0d5
Seredjja <serejatop@list.ru>
2025-03-15 17:16:55 +0300

    The first task completely ready

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,25 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
+  let d = Math.pow(b, 2) - 4 * a * c;
+  if (d === 0) {
+    let root = -b / (2 * a);
+    arr.push(root);
+  }
+  if (d > 0) {
+    let rootFirst = (-b + Math.sqrt(d)) / (2 * a);
+    let rootSecond = (-b - Math.sqrt(d)) / (2 * a);
+    arr.push(rootFirst);
+    arr.push(rootSecond);
+  }
   
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let monthlyPercent = (percent / 100) / 12;
+  let creditBody = amount - contribution;
+  let monthlyPayment = creditBody * (monthlyPercent + (monthlyPercent / ((1 + monthlyPercent)**countMonths - 1)));
+  let totalAmount = (monthlyPayment * countMonths + contribution).toFixed(2) - contribution;
+  console.log("Ввод: " + percent + ", " + contribution + ", " + amount + ", " + countMonths + ". Вывод: " + totalAmount);
 }
